f9c9ad0ff193ade393ca3f6fa8fb66b6dd1a94ee
learningDane <squeridavide@gmail.com>
2023-09-27 14:45:27 +0200

    d

--- random x casa/McD Analisi I 27.09 x 28.09/main.cpp
+++ random x casa/McD Analisi I 27.09 x 28.09/main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
-
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    int n1;
+    int n2;
+    int q;
+    int r;
+    int mcd;
+
+    start: // this is a label
+
+    cout << "Insert first number" <<endl;
+    cin >> n1;
+
+    cout<< "insert second number (must be smaller than first)" << endl;
+    cin >> n2;
+    if (n2>n1) {
+     cout<< "Second number must be smaller than first! "<< endl;
+     goto start;
+    }
+
+    
+
+    mcd:
+
+    q = n1/n2;
+    r = n1%n2;
+    if (r==0) {
+        mcd = n2;
+        cout << "the McD is " << mcd << " !" << endl;
+        return 0;
+    }
+    else goto mcd;
+
+
 }
