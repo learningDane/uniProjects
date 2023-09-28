7ee70c5e55a31deb21f8c7d62fae49e653b8fdd1
learningDane <squeridavide@gmail.com>
2023-09-28 16:27:03 +0200

    d

---
+++ FdP lezioni/28.09.2023 lab/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<< "Inserire numero da triplicare" << endl;
+    cin >> n;
+    cout << n * 3<< endl;
+    return 0;
+}
