df04a3e1b86064c93297b903bb246f16cce8a2ad
Davide Squeri <squeridavide@gmail.com>
2023-10-01 18:12:01 +0200

    F

--- tempxeseguire/main.cpp
+++ tempxeseguire/main.cpp
@@ -1,36 +1,27 @@
+// questo programma chiede un nuemro romano e ne calcola l'equivalente in base 10
 #include <iostream>
 using namespace std;
 
-int n = 0;
-int x = 0; // il numero equivalente alla lettera
-string s = "MCMXCIV";
-
-
-int check(int n1)
-{
-    if (s.at(n1) == 'M') { x = 1000; }
-    else if (s.at(n1) == 'D') { x = 500; }
-    else if (s.at(n1) == 'C') { x = 100; }
-    else if (s.at(n1) == 'L') { x = 50; }
-    else if (s.at(n1) == 'X') { x = 10; }
-    else if (s.at(n1) == 'V') { x = 5; }
-    else if (s.at(n1) == 'I') { x = 1; }
-    else { cout << "error, insert only Roman Numbers (Capitalized)" << endl; }
-    return x;
-}
-
-int main()
-{
+int main() {
+    int n = 0;
+    int x = 0; // il numero equivalente alla lettera
+    string s;
+    cout << "inserire un numero romano" << endl;
+    cin >> s;
     int tot = 0;
     int prec = 0; // il numero della lettera precedente
-    for (n = 0; n < s.length(); n++)
-    {
-        x = check(n);
-        if (prec >= x)
-        {
+    for (n = 0; n < s.length(); n++) {
+        if (s.at(n) == 'M') { x = 1000; }
+        else if (s.at(n) == 'D') { x = 500; }
+        else if (s.at(n) == 'C') { x = 100; }
+        else if (s.at(n) == 'L') { x = 50; }
+        else if (s.at(n) == 'X') { x = 10; }
+        else if (s.at(n) == 'V') { x = 5; }
+        else if (s.at(n) == 'I') { x = 1; }
+        else { cout << "error, insert only Roman Numbers (Capitalized)" << endl; }
+        if (prec >= x) {
             tot = tot + x;
-        }
-        else {tot = tot + x - 2 * prec;}
+        } else { tot = tot + x - 2 * prec; }
         prec = x;
     }
     cout << tot << endl;
