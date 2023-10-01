f1750b95ed5bdbdded0f582d2f7d3d8ec2b7e0bd
Davide Squeri <squeridavide@gmail.com>
2023-10-01 17:11:20 +0200

    d

--- tempxeseguire/main.cpp
+++ tempxeseguire/main.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
-
 using namespace std;
 
-int i = 0;
-long double n;
-long double n1;
-int j;
+int n = 0;
+int x = 0; // il numero equivalente alla lettera
+string s = "MCMXCIV";
 
-int main() {
-    cout << "inserisci primo numero" << endl;
-    cin >> n;
-    cout << "quanti decimali desideri?" << endl;
-    cin >> j;
 
-    while (i < j) {
-        n = 0.5 * (n + (2 / n));
-        cout << n << endl;
-        n1 = n;
-        i++;
+int check(int n1)
+{
+    if (s.at(n1) == 'M') { x = 1000; }
+    else if (s.at(n1) == 'D') { x = 500; }
+    else if (s.at(n1) == 'C') { x = 100; }
+    else if (s.at(n1) == 'L') { x = 50; }
+    else if (s.at(n1) == 'X') { x = 10; }
+    else if (s.at(n1) == 'V') { x = 5; }
+    else if (s.at(n1) == 'I') { x = 1; }
+    else { cout << "error, insert only Roman Numbers (Capitalized)" << endl; }
+    return x;
+}
+
+int main()
+{
+    int tot = 0;
+    int prec = 0; // il numero della lettera precedente
+    for (n = 0; n < s.length(); n++)
+    {
+        x = check(n);
+        if (prec >= x)
+        {
+            tot = tot + x;
+        }
+        else {tot = tot + x - 2 * prec;}
+        prec = x;
     }
-
-    cout << "sqrt{2} = " << n << endl;
-
+    cout << tot << endl;
 }
