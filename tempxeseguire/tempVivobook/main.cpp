f0d152e8d53b3dc1427fa8fdd21a6a3d080f27bb
learningDane <squeridavide@gmail.com>
2023-11-21 10:51:21 +0100

    jhg

--- tempxeseguire/tempVivobook/main.cpp
+++ tempxeseguire/tempVivobook/main.cpp
@@ -1,31 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-struct elem{
-    int informazione;
-    elem* punSuccessivo;
-};
-
-void aggiungiElem(elem*& primo, int newElemInf){
-    elem* temp = new elem;
-    temp-> informazione = newElemInf;
-    temp-> punSuccessivo = primo;
-    primo=temp;
-    delete temp;
-}
-
-void stampaLista(elem*&elemento,int lung) {
-    cout << elemento->informazione;
-    lung--;
-    if (lung != 0) {
-        stampaLista((elemento->punSuccessivo));
-    }
-}
-
 int main() {
-    int a = 45;
-    elem* primo = nullptr;
-    aggiungiElem(primo,a);
-    aggiungiElem(primo,85);
-    stampaLista(primo,2);
+    int n;
+    cout << "Inserisci n:" << endl;
+    cin >> n;
+    if(n < 1) {
+        cout << "Numero non valido" << endl;
+        return 0;
+    }
+    int* v = new int[n];
+    cout << "Inserisci " << n << " numeri:" << endl;
+    for(int i = 0; i < n; i++)
+        cin >> v[i];
+    int max = 0;
+    for(int i = 0; i < n; i++)
+        max = (v[i] > max) ? v[i]:max;
+    for(int i = 0; i < n; i++)
+        v[i] += max;
+    int mul = max;
+    for(int i = 0; i < n; i++)
+        mul *= v[i];
+    cout << "Risultato del calcolo:" << endl;
+    cout << mul;
+    delete[] v;
+    return 0;
 }
