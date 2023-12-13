b40ce63b8083968ec8395cdb593f665daed630cd
learningDane <squeridavide@gmail.com>
2023-12-13 14:46:06 +0100

    dsf

--- tempxeseguire/tempVivobook/main.cpp
+++ tempxeseguire/tempVivobook/main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
-#include "cinema.h"
 using namespace std;
 
+class cinema{
+    char nome[30];
+    int righe;
+    int colonne;
+    bool*posti = new bool[righe*colonne];
+public:
+    cinema(int righe , int colonne, char*nome) {
+        for (int i =0; i <30 ; i++) {
+            if (nome[i] == '\0') {
+                break;
+            }
+            else {
+                cinema::nome[i] = nome[i];
+            }
+        }
+    }
+    bool prenota(char riga, int colonna) {
+        short unsigned int intriga = riga - 'A';
+        if (cinema::posti[intriga*cinema::colonne + colonna]) { return false;}
+        cinema::posti[intriga*cinema::colonne + colonna] = true;
+        return true;
+    }
+    void stampa() {
+        for (int r = 0; r < cinema::righe; r++) {
+            for (int c = 0; c < cinema::colonne; c++) {
+
+            }
+        }
+    }
+};
+
 int main(){
-    // test prima parte
+// PRIMA PARTE
     cinema c(3,7,"Moderno");
-    c.prenota('A',4); /*
+    c.prenota('A',4);
     c.prenota('C',1);
     c.prenota('C',2);
     c.prenota('C',3);
@@ -14,12 +44,7 @@ int main(){
     c.prenota('C',6);
     c.prenota('C',7);
     c.prenota('B',4);
-    c.stampa();
+    c.stampa(); /*
     c.cancella('B',4);
-    c.stampa();
-    // test seconda parte
-    cout<<"Il numero di posti liberi in c e': "<<c.quantiLiberi()<<endl;
-    c.prenotaAdiacenti(4);
-    c.stampa();
-    return 0; */
+    c.stampa(); */
 }
