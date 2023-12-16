7b9c904eeb4a7ffd1b5ea73fe1807ada8a86d66b
learningDane <squeridavide@gmail.com>
2023-12-16 21:28:16 +0100

    tuf

--- TempEseguireDesktop/main.cpp
+++ TempEseguireDesktop/main.cpp
@@ -3,22 +3,30 @@
 using namespace std;
 class Palazzo{
 private:
-    unsigned int maxPiani;
-    struct Piano{
-        bool finestre[];
-        int*link;
-    };
-    Piano*piani[];
+    unsigned int maxPiani; //forse non serve
+    unsigned int pianiAttuali = 0;
+    bool*piani; //nel costruttore assegno poi la memoria necessaria
 public:
+
     Palazzo(int maxPiani) {
-        Palazzo::maxPiani = maxPiani;
-        Palazzo::piani = new Piano[maxPiani];
+        Palazzo::maxPiani = maxPiani; //forse non serve
+        Palazzo::piani = new bool[maxPiani]; //posso assegnare direttamente con input
+        Palazzo::pianiAttuali = 1; //il costruttore inizializza con un solo piano
+        bool*finestre = new bool[pianiAttuali]; //il piano ha (pianiAttuali)finestre, al momento della costruzione 1, perchè ha un piano
+        finestre[0] = false; //la finestra è chiusa
+        Palazzo::piani[0] = finestre; //il puntatore 0 deve puntare alle finestre del primo piano
     }
     Palazzo(Palazzo& p1) {
 
     }
     void stampa(const Palazzo Palazzo) {
-
+        cout << "Palazzo:" << endl;
+        for (int pianoAttuale = 0; pianoAttuale < Palazzo::pianiAttuali; pianoAttuale++) {
+            cout << "piano 1: ";
+            for (int finestraAttuale = 0; finestraAttuale <= pianoAttuale; finestraAttuale++) {
+                Palazzo::
+            }
+        }
     }
     void aggiungi(Palazzo Palazzo) {
 
