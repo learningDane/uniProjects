ceea113ccda68d9b41b0b4086015ed7d522f54c1
learningDane <squeridavide@gmail.com>
2023-12-18 19:31:19 +0100

    ciao

--- tempVivobook/compito.cpp
+++
@@ -1,9 +0,0 @@
-#include "compito.h"
-
-void Palazzo(unsigned int N) {
-    Palazzo:piani = N;
-}
-
-void Palazzo::Palazzo(const Palazzo& p1) {
-
-}
--- tempVivobook/main.cpp
+++ tempVivobook/main.cpp
@@ -5,27 +5,36 @@ class Palazzo{
 private:
     unsigned int maxPiani; //forse non serve
     unsigned int pianiAttuali = 0;
-    bool*piani; //nel costruttore assegno poi la memoria necessaria
+    bool**finestre; //nel costruttore assegno poi la memoria necessaria
 public:
 
     Palazzo(int maxPiani) {
         Palazzo::maxPiani = maxPiani; //forse non serve
-        Palazzo::piani = new bool[maxPiani]; //posso assegnare direttamente con input
+        Palazzo::finestre = new bool*[maxPiani]; //posso assegnare direttamente con input
         Palazzo::pianiAttuali = 1; //il costruttore inizializza con un solo piano
-        bool*finestre = new bool[pianiAttuali]; //il piano ha (pianiAttuali)finestre, al momento della costruzione 1, perchè ha un piano
-        finestre[0] = false; //la finestra è chiusa
-        Palazzo::piani[0] = finestre; //il puntatore 0 deve puntare alle finestre del primo piano
+        Palazzo::finestre[0] = new bool[pianiAttuali]; //il piano ha (pianiAttuali)finestre, al momento della costruzione 1, perchè ha un piano
+        finestre[0][0] = false; //la finestra è chiusa
     }
     Palazzo(Palazzo& p1) {
-
+        Palazzo::maxPiani = p1.maxPiani;
+        Palazzo::finestre = new bool*[maxPiani];
+        Palazzo::pianiAttuali = p1.pianiAttuali;
+        //adesso copio piano per piano (piano = finestre[x])
+        for (int i = 0; i < Palazzo::pianiAttuali; i++) { //i=piano attuale (il primo piano e a i = 0)
+            Palazzo::finestre[i] = new bool[i+1];
+            for (int j = 0; j < i; j++) {
+                Palazzo::finestre[i][j] = p1.finestre[i][j];
+            }
+        }
     }
-    void stampa(const Palazzo Palazzo) {
+    void stampa() {
         cout << "Palazzo:" << endl;
         for (int pianoAttuale = 0; pianoAttuale < Palazzo::pianiAttuali; pianoAttuale++) {
-            cout << "piano 1: ";
+            cout << "piano " << pianoAttuale + 1 << ": ";
             for (int finestraAttuale = 0; finestraAttuale <= pianoAttuale; finestraAttuale++) {
-                cout << Palazzo::piani[pianoAttuale][finestraAttuale] ? "aperta " : "chiusa ";
+                cout << (Palazzo::finestre[pianoAttuale][finestraAttuale] ? "aperta " : "chiusa ");
             }
+            cout << endl;
         }
     }
     void aggiungi(Palazzo Palazzo) {
@@ -36,13 +45,13 @@ int main()
 {
     // PRIMA PARTE:
     cout << "Test del costruttore:" << endl;
-    Palazzo p1(5); /*
+    Palazzo p1(5);
     p1.stampa();
     cout << endl;
     cout << "Test del costruttore di copia:" << endl;
     Palazzo p2(p1);
     p2.stampa();
-    cout << endl;
+    cout << endl; /*
     cout << "Test della aggiungi:" << endl;
     p1.aggiungi();
     p1.aggiungi();
