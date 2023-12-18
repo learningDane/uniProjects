9eb8dd069bb8e8ad4a4cd07f24413dfb41ab5cd2
learningDane <squeridavide@gmail.com>
2023-12-18 19:04:30 +0100

    sdv

--- tempVivobook/main.cpp
+++ tempVivobook/main.cpp
@@ -1,12 +1,42 @@
 //#include "compito.h"
 #include <iostream>
 using namespace std;
-class
+class Palazzo{
+private:
+    unsigned int maxPiani; //forse non serve
+    unsigned int pianiAttuali = 0;
+    bool*piani; //nel costruttore assegno poi la memoria necessaria
+public:
+
+    Palazzo(int maxPiani) {
+        Palazzo::maxPiani = maxPiani; //forse non serve
+        Palazzo::piani = new bool[maxPiani]; //posso assegnare direttamente con input
+        Palazzo::pianiAttuali = 1; //il costruttore inizializza con un solo piano
+        bool*finestre = new bool[pianiAttuali]; //il piano ha (pianiAttuali)finestre, al momento della costruzione 1, perchè ha un piano
+        finestre[0] = false; //la finestra è chiusa
+        Palazzo::piani[0] = finestre; //il puntatore 0 deve puntare alle finestre del primo piano
+    }
+    Palazzo(Palazzo& p1) {
+
+    }
+    void stampa(const Palazzo Palazzo) {
+        cout << "Palazzo:" << endl;
+        for (int pianoAttuale = 0; pianoAttuale < Palazzo::pianiAttuali; pianoAttuale++) {
+            cout << "piano 1: ";
+            for (int finestraAttuale = 0; finestraAttuale <= pianoAttuale; finestraAttuale++) {
+                cout << Palazzo::piani[pianoAttuale][finestraAttuale] ? "aperta " : "chiusa ";
+            }
+        }
+    }
+    void aggiungi(Palazzo Palazzo) {
+
+    }
+};
 int main()
 {
     // PRIMA PARTE:
     cout << "Test del costruttore:" << endl;
-    Palazzo p1(5);
+    Palazzo p1(5); /*
     p1.stampa();
     cout << endl;
     cout << "Test del costruttore di copia:" << endl;
