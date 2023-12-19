6958a59db7c8694056a5ce48d168d7de094e84fd
learningDane <squeridavide@gmail.com>
2023-12-19 19:42:45 +0100

    f

--- TempEseguireDesktop/main.cpp
+++ TempEseguireDesktop/main.cpp
@@ -16,11 +16,16 @@ public:
         finestre[0][0] = false; //la finestra è chiusa
     }
     Palazzo(Palazzo& p1) {
-        Palazzo::maxPiani = p1.maxPiani; //forse non serve
-        Palazzo::finestre = new bool*[maxPiani]; //posso assegnare direttamente con input
-        Palazzo::pianiAttuali = 1; //il costruttore inizializza con un solo piano
-        Palazzo::finestre[0] = new bool[pianiAttuali]; //il piano ha (pianiAttuali)finestre, al momento della costruzione 1, perchè ha un piano
-        finestre[0][0] = false; //la finestra è chiusa
+        Palazzo::maxPiani = p1.maxPiani;
+        Palazzo::finestre = new bool*[maxPiani];
+        Palazzo::pianiAttuali = p1.pianiAttuali;
+        //adesso copio piano per piano (piano = finestre[x])
+        for (int i = 0; i < Palazzo::pianiAttuali; i++) { //i=piano attuale (il primo piano e a i = 0)
+            Palazzo::finestre[i] = new bool[i+1];
+            for (int j = 0; j < i; j++) {
+                Palazzo::finestre[i][j] = p1.finestre[i][j];
+            }
+        }
     }
     void stampa() {
         cout << "Palazzo:" << endl;
@@ -32,8 +37,51 @@ public:
             cout << endl;
         }
     }
-    void aggiungi(Palazzo Palazzo) {
+    bool aggiungi() {
+        if (Palazzo::pianiAttuali == Palazzo::maxPiani) {
+            return false;
+        }
+        Palazzo::finestre[Palazzo::pianiAttuali] = new bool[Palazzo::pianiAttuali];
+        for (int i = 0; i < Palazzo::pianiAttuali + 1; i++) {
+            finestre[pianiAttuali][i] = false;
+        }
+        Palazzo::pianiAttuali++;
+        return true;
+    }
 
+    void cambia(const unsigned int piano, const unsigned int finestra) {
+        Palazzo::finestre[piano - 1][finestra - 1] = !Palazzo::finestre[piano - 1][finestra - 1];
+    }
+
+    int operator!(){
+        unsigned int finestreAperte = 0;
+        for (int pianoAttuale = 0; pianoAttuale < Palazzo::pianiAttuali; pianoAttuale++) {
+            for (int finestraAttuale = 0; finestraAttuale <= pianoAttuale; finestraAttuale++) {
+                if (Palazzo::finestre[pianoAttuale][finestraAttuale]) {
+                    finestreAperte++;
+                }
+            }
+        }
+        return finestreAperte;
+    }
+
+    void operator%=(const Palazzo& other) {
+        if (Palazzo::pianiAttuali == other.pianiAttuali) {return;}
+        for (int pianoAttuale = 0; pianoAttuale < Palazzo::pianiAttuali; pianoAttuale++) {
+            for (int finestraAttuale = 0; finestraAttuale <= pianoAttuale; finestraAttuale++) {
+                //if (!Palazzo::finestre[pianoAttuale][finestraAttuale]) {
+                //    other.finestre[pianoAttuale][finestraAttuale] = false;
+                //}
+                finestre[0][0]= false;
+            }
+        }
+    }
+
+    virtual ~Palazzo() {
+        for (int i = 0; i < pianiAttuali; i++) {
+            delete[] Palazzo::finestre[i];
+        }
+        delete[] Palazzo::finestre;
     }
 };
 int main()
@@ -41,7 +89,7 @@ int main()
     // PRIMA PARTE:
     cout << "Test del costruttore:" << endl;
     Palazzo p1(5);
-    p1.stampa(); /*
+    p1.stampa();
     cout << endl;
     cout << "Test del costruttore di copia:" << endl;
     Palazzo p2(p1);
@@ -53,7 +101,7 @@ int main()
     p1.stampa();
     cout << endl;
     // SECONDA PARTE:
-    /*cout << "Test del distruttore:" << endl;
+    cout << "Test del distruttore:" << endl;
     {
     Palazzo p(20);
     }
@@ -72,6 +120,6 @@ int main()
     p3.aggiungi();
     p1 %= p3;
     p1.stampa();
-    cout << endl;*/
+    cout << endl;
     return 0;
 }
